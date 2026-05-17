b8fd0d5f06cd0576d1a89d2964655801566fcb72
somali-index <zzzam12345678910@gmail.com>
2026-05-17 22:24:59 +0700

    test

--- app/src/main/java/com/example/flickfind/DATALAYER/Room/RoomMovies.kt
+++ app/src/main/java/com/example/flickfind/DATALAYER/Room/RoomMovies.kt
@@ -1,16 +1,4 @@
 package com.example.flickfind.DATALAYER.Room
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "movieData")
-
-data class RoomMovies(
-    @PrimaryKey
-    val IDMovie: String,
-
-    val NameMovie: String,
-    val Description: String,
-    val IDStudio: String,
-    val URLimage: String
-)
+class RoomMovies {
+}
