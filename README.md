c433e11cf1cfaa6086b9bad892da044a37f74c66
somali-index <zzzam12345678910@gmail.com>
2026-06-15 14:28:56 +0700

    clear old code

--- README.md
+++
@@ -1,3 +0,0 @@
-# 🎬 FlickFind 
-
-CHÀO MỪNG ĐẾN VỚI ỨNG DỤNG TÌM PHIM 
--- app/src/main/java/com/example/flickfind/DATALAYER/DataClass/DataCollection.kt
+++
@@ -1,8 +0,0 @@
-package com.example.flickfind.DATALAYER.DataClass
-
-data class DataCollection(
-    val IDCollection: String = "",
-    val IDUser: String = "",
-    val CollectionName: String = "",
-    val CreateAt: Long = System.currentTimeMillis()
-)
